c3ca087e46e73b5966881cd5b1e792b80bb74b1c
kumauta <nogami.s@gmail.com>
2015-10-01 14:14:07 +0900

    Add settings

--- lib/build_pack_utils/downloads.py
+++ lib/build_pack_utils/downloads.py
@@ -36,6 +36,7 @@ class Downloader(object):
         self._log.info('Downloaded [%s] to [%s]', url, toFile)
 
     def download_direct(self, url):
+        print '############ To Download [%s]' % (url)
         buf = urllib2.urlopen(url).read()
         self._log.info('Downloaded [%s] to memory', url)
         self._log.debug("Downloaded [%s] [%s]", url, buf)
